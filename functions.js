5a2036c94a2e6a590d3589469c248e72f908113f
Housam-j <Housam-j@hotmail.com>
2021-09-03 03:56:10 +0000

    -a

---
+++ functions.js
@@ -0,0 +1,18 @@
+function eat(food){
+  return food + ' tasted really good.' 
+}
+console.log(eat('bananasfunction smartGarbage(trash, bins){
+const keys = Object.keys(bins)
+
+for ( let keys = 0; keys < keys.length; keys++)
+{
+if (keys == recycling)
+{
+  
+bins += 1;
+
+}
+}
+return (bins);
+
+}'))
---
+++ looping-through-arrays.js
@@ -0,0 +1,7 @@
+var pets=['cat', 'dog', 'rat']
+var i=0;
+while (i < pets.length ) {
+  pets[i]=pets[i]+"s";
+  i=i+1
+}
+console.log (pets)
